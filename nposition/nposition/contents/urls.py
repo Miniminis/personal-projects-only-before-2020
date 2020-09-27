11028f733cb03bc47220cceab42f19cf581a6917
miniminis <minhee4735@gmail.com>
2020-09-28 03:46:42 +0900

    nposition: 각 메뉴별 리스트 화면 markup 작업 완료

--- nposition/nposition/contents/urls.py
+++ nposition/nposition/contents/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
-from .views import MentoView
+from .views import (MentoView, 
+                    LectureView, 
+                    MenteeWelcomeView, MenteeProfitAuthView, MenteeProductRecommView, 
+                    MenteeBoardView, MenteeQnAView,
+                    AnnouncementView, EventView, FAQView)
+
 
 urlpatterns = [
     path('mento/', MentoView.as_view(), name='mento'),
+
+    path('lecture/', LectureView.as_view(), name='lecture'),
+
+    path('mentee/welcome', MenteeWelcomeView.as_view(), name='welcome'),
+    path('mentee/profit', MenteeProfitAuthView.as_view(), name='profit'),
+    path('mentee/product', MenteeProductRecommView.as_view(), name='product'),
+    path('mentee/qna', MenteeQnAView.as_view(), name='qna'),
+    path('mentee/board', MenteeBoardView.as_view(), name='board'),
+
+    path('announce/', AnnouncementView.as_view(), name='announce'),
+    path('event/', EventView.as_view(), name='event'),
+    path('faq/', FAQView.as_view(), name='faq'),
 ]
--- nposition/nposition/contents/views.py
+++ nposition/nposition/contents/views.py
@@ -15,3 +15,29 @@ class LoginView(TemplateView):
 class MentoView(TemplateView):
     template_name='mento/mento.html'
 
+class LectureView(TemplateView):
+    template_name='lecture/lecture.html'
+
+class MenteeWelcomeView(TemplateView):
+    template_name='mentee/welcome.html'
+
+class MenteeProfitAuthView(TemplateView):
+    template_name='mentee/profit_auth.html'
+
+class MenteeProductRecommView(TemplateView):
+    template_name='mentee/product_recomm.html'
+
+class MenteeQnAView(TemplateView):
+    template_name='mentee/q_and_a.html'
+
+class MenteeBoardView(TemplateView):
+    template_name='mentee/board.html'
+
+class AnnouncementView(TemplateView):
+    template_name='etc/announcement.html'
+
+class EventView(TemplateView):
+    template_name='etc/event.html'
+
+class FAQView(TemplateView):
+    template_name='etc/faq.html'
